c4dd05c1d70dbb4e8fe49cf4a0919f1757c2c6ef
Diogo Peixoto <diogosoarespeixoto41@gmail.com>
2022-02-08 05:21:08 -0300

    myfinance

---
+++ Back-end/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+
+
+app.get('/api/transactions',()=>{
+
+})
+
+app.post('/api/transactions', ()=>{
+    
+})
+
+app.listen(3000, ()=>{
+    console.log("Server Running")
+})
